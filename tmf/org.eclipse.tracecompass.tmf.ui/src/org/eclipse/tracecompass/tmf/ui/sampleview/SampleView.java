66d639d343e0c64cd271b8a890e8aced28b2b5b1
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-06-29 00:47:52 -0400

    SampleView extending from Call stack view, it works but the line is not shown

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/sampleview/SampleView.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/sampleview/SampleView.java
@@ -2,16 +2,13 @@ package org.eclipse.tracecompass.tmf.ui.sampleview;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.CCTAnalysisModule;
 import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.IProfileData;
@@ -19,20 +16,12 @@ import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.IProfile
 import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.Node;
 import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.ProfileData;
 import org.eclipse.tracecompass.internal.analysis.os.linux.core.profile.ProfileTraversal.KeyTree;
-import org.eclipse.tracecompass.internal.tmf.ui.Activator;
-import org.eclipse.tracecompass.internal.tmf.ui.Messages;
+import org.eclipse.tracecompass.statesystem.core.ITmfStateSystem;
 import org.eclipse.tracecompass.tmf.core.analysis.IAnalysisModule;
-import org.eclipse.tracecompass.tmf.core.signal.TmfWindowRangeUpdatedSignal;
-import org.eclipse.tracecompass.tmf.core.timestamp.ITmfTimestamp;
-import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimeRange;
-import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimestamp;
-import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimestampDelta;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.core.trace.TmfTraceUtils;
-import org.eclipse.tracecompass.tmf.ui.views.callstack.CallStackEntry;
-import org.eclipse.tracecompass.tmf.ui.views.callstack.CallStackPresentationProvider;
-import org.eclipse.tracecompass.tmf.ui.views.timegraph.AbstractTimeGraphView;
-import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.TimeGraphContentProvider;
+import org.eclipse.tracecompass.tmf.ui.views.callstack.AbstractCallStackAnalysis;
+import org.eclipse.tracecompass.tmf.ui.views.callstack.CallStackView;
 import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.ITimeEvent;
 import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.ITimeGraphEntry;
 import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.TimeEvent;
@@ -61,8 +50,7 @@ import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.TimeGraphEntry;
  * graph view on the right side
  */
 
-public class SampleView extends AbstractTimeGraphView {// extends CallStackView
-                                                       // { //
+public class SampleView extends CallStackView {
 
     /**
      * The ID of the view as specified by the extension.
@@ -70,35 +58,13 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
     public static final String ID1 = "org.eclipse.tracecompass.tmf.ui.views.SampleView";
     private Node<ProfileData> fRoot;
 
-    private static final String[] COLUMN_NAMES1 = new String[] {
-            Messages.CallStackView_FunctionColumn,
-            Messages.CallStackView_DepthColumn,
-            Messages.CallStackView_EntryTimeColumn,
-            Messages.CallStackView_ExitTimeColumn,
-            Messages.CallStackView_DurationColumn
-    };
-
-    private static final String[] FILTER_COLUMN_NAMES = new String[] {
-            Messages.CallStackView_ThreadColumn
-    };
-
     /**
      * The constructor.
      */
     public SampleView() {
-        // super();
-        super(ID1, new CallStackPresentationProvider());
-        ((CallStackPresentationProvider) getPresentationProvider()).setSampleView(this);
-        setTreeColumns(COLUMN_NAMES1);
-        setTreeLabelProvider(new SampleViewTreeLabelProvider());
-        setEntryComparator(new SampleViewComparator());
-        setFilterColumns(FILTER_COLUMN_NAMES);
-        setFilterContentProvider(new SampleViewFilterContentProvider());
-        setFilterLabelProvider(new SampleViewTreeLabelProvider());
-
+        super();
+        //getTimeGraphViewer().setStartFinishTime(0, 100);
         setHandleTimeSignals(false);
-        setStartTime(0);
-        setEndTime(15);
     }
 
     /**
@@ -108,9 +74,18 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
     // Override this method:
     @Override
     protected void buildEntryList(final ITmfTrace trace, final ITmfTrace parentTrace, final IProgressMonitor monitor) {
+        // Get State System:
+        AbstractCallStackAnalysis moduleToSSTree = getCallStackModule(trace);
+        if (moduleToSSTree == null) {
+            return;
+        }
+        ITmfStateSystem ss = moduleToSSTree.getStateSystem();
+
+
         System.out.println("buildEntryList " + trace.getName());
         Iterable<CCTAnalysisModule> iter = TmfTraceUtils.getAnalysisModulesOfClass(trace, CCTAnalysisModule.class);
         CCTAnalysisModule module = null;
+
 
         // Selects only the CCTAnalysis module
         for (IAnalysisModule mod : iter) {
@@ -128,18 +103,9 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
         module.schedule();
         module.waitForCompletion();
 
-        // Get the data from the module
+        // Take the tree and put as fRoot:
         Node<ProfileData> root = module.getTree();
         fRoot = root;
-        // Find the beginning and end of this view and set it using (i think)
-        // getTimeGraphWrapper or something.getTimeGraphViewer.setStart...
-        //getTimeGraphViewer().setStartFinishTime(0, 10);
-
-        // Build your entries
-
-        // For each entry, get the list of events
-
-        // enjoy the show
 
         // For the View:
         TraceEntry traceEntry = null;
@@ -149,19 +115,21 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
         // Map of the nodes:
         Map<KeyTree, Node<ProfileData>> map;
 
-        long startTime = 0; // fRoot.getProfileData().getStartTime();
+        long startTime = fRoot.getProfileData().getStartTime();
         long start = startTime;
         setStartTime(Math.min(getStartTime(), startTime));
 
         if (monitor.isCanceled()) {
             return;
         }
-        long end = 15;// fRoot.getProfileData().getEndTime();
+        long end = fRoot.getProfileData().getEndTime();
         long endTime = end + 1;
+
 
         setEndTime(Math.max(getEndTime(), endTime));
 
-        traceEntry = new TraceEntry(trace.getName(), startTime, endTime);
+        System.out.println(ss.getStartTime());
+        traceEntry = new TraceEntry(trace.getName(), ss.getStartTime(), ss.getCurrentEndTime());
         addToEntryList(parentTrace, Collections.singletonList(traceEntry));
 
         System.out.println("Tree");
@@ -171,10 +139,12 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
         EventEntry eventEntryAux = null;
 
         // Creating the LevelEntry (key is the level)
-        levelEntryAux = new LevelEntry("foo", 0, 0, 10);
+        levelEntryAux = new LevelEntry("level 0", 0, ss.getStartTime() + 10, ss.getCurrentEndTime() - 2); //fRoot.getProfileData().getStartTime(), fRoot.getProfileData().getEndTime() + 1);
+        traceEntry.addChild(levelEntryAux);
+        levelEntryMap.put(trace, levelEntryAux);
 
         // Creating a eventEntry
-        eventEntryAux = new EventEntry("main", 37, 1, 8);
+        eventEntryAux = new EventEntry("function 1", 37, ss.getStartTime() + 10, ss.getCurrentEndTime() - 2); //fRoot.getProfileData().getStartTime() + 1, fRoot.getProfileData().getEndTime());
 
         // Put as child
         List<ITimeEvent> eventList = new ArrayList<>(4);
@@ -186,16 +156,13 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
 
         levelEntryAux.addChild(eventEntryAux);
 
-        traceEntry.addChild(levelEntryAux);
-        levelEntryMap.put(trace, levelEntryAux);
-
         start = end;
     }
 
     /**
      * This method creates the status of the Events
      *
-     * @param entry
+     * @param entry:
      *            Level Entry
      * @param startTime
      * @param endTime
@@ -204,7 +171,6 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
      * @param root
      * @return
      */
-
     protected @Nullable List<ITimeEvent> getEventList(TimeGraphEntry entry, long resolution, IProgressMonitor monitor, Map<KeyTree, Node<ProfileData>> map) {
 
         LevelEntry queueNodesEntry = (LevelEntry) entry;
@@ -264,26 +230,9 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
 
     }
 
-    /*
-     * Maybe change this method here:
-     *
-     * @Override public void createPartControl(Composite parent) {
-     * super.createPartControl(parent);
-     *
-     * }
-     *
-     */
-    // This function creates the widgets on the screen
     @Override
     public void createPartControl(Composite parent) {
-        // This will give problem, because the sample view extends call stack
-        // view
         super.createPartControl(parent);
-
-        TmfTimeRange range = new TmfTimeRange(TmfTimestamp.fromNanos(0), TmfTimestamp.fromNanos(15));
-        broadcast(new TmfWindowRangeUpdatedSignal(SampleView.this, range));
-        getTimeGraphViewer().setStartFinishTime(0, 15);
-
     }
 
     // TraceEntry is a trace
@@ -299,7 +248,7 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
     }
 
     // LevelEntry is an Level on the tree
-    private static class LevelEntry extends TimeGraphEntry implements Comparable<LevelEntry> {
+    private static class LevelEntry extends TimeGraphEntry {
 
         private final int fLevelId;
 
@@ -316,20 +265,10 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
         public int getLevel() {
             return fLevelId;
         }
-
-        @Override
-        public int compareTo(LevelEntry obj) {
-            if (this.fLevelId == obj.fLevelId) {
-                return 0;
-            }
-
-            return Integer.compare(this.fLevelId, obj.fLevelId);
-
-        }
     }
 
     // EventEntry is an Node on the tree
-    private static class EventEntry extends TimeGraphEntry implements Comparable<EventEntry> {
+    private static class EventEntry extends TimeGraphEntry {
 
         private final int fNodeId;
 
@@ -345,16 +284,6 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
 
         public int getNodeId() {
             return fNodeId;
-        }
-
-        @Override
-        public int compareTo(EventEntry obj) {
-            if (this.fNodeId == obj.fNodeId) {
-                return 0;
-            }
-
-            return Integer.compare(this.fNodeId, obj.fNodeId);
-
         }
     }
 
@@ -456,100 +385,4 @@ public class SampleView extends AbstractTimeGraphView {// extends CallStackView
         return hmap;
     }
 
-
-    // Copied methods:
-    private static final Image PROCESS_IMAGE = Activator.getDefault().getImageFromPath("icons/obj16/process_obj.gif"); //$NON-NLS-1$
-    private static final Image LEVEL_IMAGE = Activator.getDefault().getImageFromPath("icons/obj16/thread_obj.gif"); //$NON-NLS-1$
-    private static final Image EVENT_STACKFRAME_IMAGE = Activator.getDefault().getImageFromPath("icons/obj16/stckframe_obj.gif"); //$NON-NLS-1$
-
-    private static class SampleViewTreeLabelProvider extends TreeLabelProvider {
-
-        @Override
-        public Image getColumnImage(Object element, int columnIndex) {
-            if (columnIndex == 0) {
-                if (element instanceof TraceEntry) {
-                    return PROCESS_IMAGE;
-                } else if (element instanceof LevelEntry) {
-                    return LEVEL_IMAGE;
-                } else if (element instanceof EventEntry) {
-                    CallStackEntry entry = (CallStackEntry) element;
-                    if (entry.getFunctionName().length() > 0) {
-                        return EVENT_STACKFRAME_IMAGE;
-                    }
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public String getColumnText(Object element, int columnIndex) {
-            if (element instanceof CallStackEntry) {
-                CallStackEntry entry = (CallStackEntry) element;
-                if (columnIndex == 0) {
-                    return entry.getFunctionName();
-                } else if (columnIndex == 1 && entry.getFunctionName().length() > 0) {
-                    int depth = entry.getStackLevel();
-                    return Integer.toString(depth);
-                } else if (columnIndex == 2 && entry.getFunctionName().length() > 0) {
-                    ITmfTimestamp ts = TmfTimestamp.fromNanos(entry.getFunctionEntryTime());
-                    return ts.toString();
-                } else if (columnIndex == 3 && entry.getFunctionName().length() > 0) {
-                    ITmfTimestamp ts = TmfTimestamp.fromNanos(entry.getFunctionExitTime());
-                    return ts.toString();
-                } else if (columnIndex == 4 && entry.getFunctionName().length() > 0) {
-                    ITmfTimestamp ts = new TmfTimestampDelta(entry.getFunctionExitTime() - entry.getFunctionEntryTime(), ITmfTimestamp.NANOSECOND_SCALE);
-                    return ts.toString();
-                }
-            } else if (element instanceof ITimeGraphEntry) {
-                if (columnIndex == 0) {
-                    return ((ITimeGraphEntry) element).getName();
-                }
-            }
-            return ""; //$NON-NLS-1$
-        }
-
-    }
-
-    private class SampleViewComparator implements Comparator<ITimeGraphEntry> {
-        @Override
-        public int compare(ITimeGraphEntry o1, ITimeGraphEntry o2) {
-            // return o1.compareTo(o2);
-
-            if (o1 instanceof EventEntry && o2 instanceof EventEntry) {
-                EventEntry first = (EventEntry) o1;
-                EventEntry second = (EventEntry) o1;
-                return first.compareTo(second);
-            } else if (o1 instanceof LevelEntry && o2 instanceof LevelEntry) {
-                LevelEntry first = (LevelEntry) o1;
-                LevelEntry second = (LevelEntry) o1;
-                return first.compareTo(second);
-            }
-            return 0;
-        }
-    }
-
-    private class SampleViewFilterContentProvider extends TimeGraphContentProvider {
-        @Override
-        public boolean hasChildren(Object element) {
-            if (element instanceof TraceEntry) {
-                return super.hasChildren(element);
-            }
-            return false;
-        }
-
-        @Override
-        public ITimeGraphEntry[] getChildren(Object parentElement) {
-            if (parentElement instanceof TraceEntry) {
-                return super.getChildren(parentElement);
-            }
-            return new ITimeGraphEntry[0];
-        }
-    }
-
-    @Override
-    protected @Nullable List<@NonNull ITimeEvent> getEventList(@NonNull TimeGraphEntry entry, long startTime, long endTime, long resolution, @NonNull IProgressMonitor monitor) {
-
-
-        return null;
-    }
 }
