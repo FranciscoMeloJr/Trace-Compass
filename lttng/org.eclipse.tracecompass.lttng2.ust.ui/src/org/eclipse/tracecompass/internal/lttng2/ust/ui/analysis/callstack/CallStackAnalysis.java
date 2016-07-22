cd41c3c5bfb18cd6e6e84e1f8cb8c75e38895b18
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-07-22 18:24:27 -0400

    current versio nof FlameGraph with CallStackAnalysis

--- lttng/org.eclipse.tracecompass.lttng2.ust.ui/src/org/eclipse/tracecompass/internal/lttng2/ust/ui/analysis/callstack/CallStackAnalysis.java
+++ lttng/org.eclipse.tracecompass.lttng2.ust.ui/src/org/eclipse/tracecompass/internal/lttng2/ust/ui/analysis/callstack/CallStackAnalysis.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 
