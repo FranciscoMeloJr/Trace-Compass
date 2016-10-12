ed20b968f9e53156153fe84d5483a532fb691d34
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-10-11 21:58:21 -0400

    TestStatistic: add calculateCorrelation function. CCTAnalysis: correlationInfoTrace

--- analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/CCTAnalysisModule.java
+++ analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/CCTAnalysisModule.java
@@ -1910,71 +1910,7 @@ public class CCTAnalysisModule extends TmfAbstractAnalysisModule {
         }
 
         //call the correlation:
-        calculateCorrelation(durationList, traceInfo);
+        TestStatistic.calculateCorrelation(durationList, traceInfo);
     }
 
-    // Calculates the correlation of two distributions:
-    public static void calculateCorrelation(ArrayList<Double> data1, ArrayList<Double> data2) {
-
-        //Ordering:
-        Collections.sort(data1);
-        Collections.sort(data2);
-
-        ArrayList<Double> x = new ArrayList<>();
-        ArrayList<Double> xx = new ArrayList<>();
-        ArrayList<Double> y = new ArrayList<>();
-        ArrayList<Double> yy = new ArrayList<>();
-
-        ArrayList<Double> xy = new ArrayList<>();
-
-        Double mean1 = (double) 0;
-        Double mean2 = (double) 0;
-        Double value = (double) 0;
-        Double value2 = (double) 0;
-
-        // total:
-        Double total1 = (double) 0;
-        Double total2 = (double) 0;
-        Double total3 = (double) 0;
-
-        int minSize = (data1.size() < data2.size()) ? data1.size() : data2.size();
-
-        for (int j = 0; j < minSize; j++) {
-            mean1 += data1.get(j);
-        }
-        mean1 /= data1.size();
-        for (int j = 0; j < minSize; j++) {
-            value = data1.get(j) - mean1;
-            x.add(value);
-            value2 = value * value;
-            xx.add(value2);
-            total2 += value2;
-        }
-
-        for (int j = 0; j < minSize; j++) {
-            mean2 += data2.get(j);
-        }
-        mean2 /= data2.size();
-
-        for (int j = 0; j < minSize; j++) {
-            value = data2.get(j) - mean2;
-            y.add(value);
-            value2 = value * value;
-            yy.add(value2);
-            total3 += value2;
-        }
-
-
-        for (int j = 0; j < minSize; j++) {
-            xy.add(x.get(j) * y.get(j));
-        }
-        for (int j = 0; j < xy.size(); j++) {
-            total1 += xy.get(j);
-        }
-
-        //Showing Pearson correlation:
-        Double result = (total1 / (total2 * total3));
-        System.out.format(" Correlation %.10f%n", result);
-
-    }
 }
