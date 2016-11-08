37d649d56c25a932f1dce062a2ec7a1284d30d56
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-11-07 20:14:21 -0500

    NormalTests: elapsed time = cpu time *2. Trying to track the cause of the elapsed time using the optk-means

--- analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/NormalTests.java
+++ analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/NormalTests.java
@@ -29,6 +29,17 @@ public class NormalTests {
 
         Executions() {
             id = 1;
+
+            fulfilDistributionET();
+
+            fulfilDistributionCPU();
+
+            fulfilDistributionWrite();
+
+            runs = createsRuns();
+        }
+
+        private void fulfilDistributionET() {
             // Elapsed Time:
             int stdDev = 15;
             int mean = 20;
@@ -42,14 +53,12 @@ public class NormalTests {
             stdDev = 15;
             mean = 70;
             fulfil(distributionET, hmET, stdDev, mean, 2);
+        }
 
-            // CPU Time
-            stdDev = 15;
-            mean = 20;
-
+        private void fulfilDistributionCPU() {
             // first Normal:
-            stdDev = 15;
-            mean = 10;
+            int stdDev = 15;
+            int mean = 10;
             fulfil(distributionCPU, hmCPU, stdDev, mean, 1);
 
             // second Normal:
@@ -57,13 +66,12 @@ public class NormalTests {
             mean = 70;
             fulfil(distributionCPU, hmCPU, stdDev, mean, 2);
 
-            // Write
-            stdDev = 15;
-            mean = 20;
+        }
 
+        private void fulfilDistributionWrite() {
             // first Normal:
-            stdDev = 15;
-            mean = 10;
+            int stdDev = 15;
+            int mean = 10;
             fulfil(distributionWrite, hmWrite, stdDev, mean, 1);
 
             // second Normal:
@@ -71,7 +79,6 @@ public class NormalTests {
             mean = 70;
             fulfil(distributionWrite, hmWrite, stdDev, mean, 2);
 
-            runs = createsRuns();
         }
 
         void fulfil(ArrayList<Double> distribution, LinkedHashMap<Integer, ArrayList<Double>> hm, int stdDev, int mean, int type) {
@@ -91,8 +98,25 @@ public class NormalTests {
         ArrayList<Run> createsRuns() {
             ArrayList<Run> arrayR = new ArrayList<>();
             Run tempR;
-            for (int i = 0; i < tam; i++) {
-                tempR = new Run(i, distributionET.get(i), distributionCPU.get(i), distributionWrite.get(i));
+
+            ArrayList<Integer> arrayLocation1 = new ArrayList<>();
+            ArrayList<Integer> arrayLocation2 = new ArrayList<>();
+            ArrayList<Integer> arrayLocation3 = new ArrayList<>();
+
+            // put
+            for (int i = 0; i < tam * 2; i++) {
+                arrayLocation1.add(i);
+                arrayLocation2.add(i);
+                arrayLocation3.add(i);
+            }
+
+            // shuffle
+            Collections.shuffle(arrayLocation1);
+            Collections.shuffle(arrayLocation2);
+            Collections.shuffle(arrayLocation3);
+
+            for (int i = 0; i < tam * 2; i++) {
+                tempR = new Run(i, distributionCPU.get(arrayLocation1.get(i)), distributionET.get(arrayLocation2.get(i)), distributionWrite.get(arrayLocation3.get(i)));
                 arrayR.add(tempR);
             }
             return arrayR;
@@ -126,15 +150,20 @@ public class NormalTests {
             }
         }
 
+        // This function returns the runs:
+        ArrayList<Run> getRuns() {
+            return runs;
+        }
+
         // Function used to shuffle the runs information:
-        void suffle() {
+        void shuffle() {
             if (distributionRandom != null) {
                 Collections.sort(distributionRandom);
             }
 
-            Collections.sort(distributionET);
-            Collections.sort(distributionCPU);
-            Collections.sort(distributionWrite);
+            Collections.shuffle(distributionET);
+            Collections.shuffle(distributionCPU);
+            Collections.shuffle(distributionWrite);
         }
 
         // getters of the hash:
@@ -162,6 +191,24 @@ public class NormalTests {
         ArrayList<Double> getElapsed() {
             return distributionET;
         }
+
+        // This function run through the runs and connect the distributions (cpu
+        // time and elapsed time):
+        public void connect() {
+
+            distributionET = new ArrayList<>();
+            // run:
+            for (int i = 0; i < runs.size(); i++) {
+                Run eachRun = runs.get(i);
+                Double cpu = eachRun.getCpu();
+                eachRun.setElapsed(cpu * 2);
+                distributionET.add(cpu * 2);
+
+                // hashAdd(hm, type, aux);
+
+            }
+
+        }
     }
 
     // Each execution:
@@ -172,6 +219,10 @@ public class NormalTests {
 
         int id;
 
+        int GCpu;
+        int GElapsed;
+        int GWrite;
+
         Run(int identification, double c, double e, double w) {
             this.cpu = c;
             this.elapsed = e;
@@ -179,6 +230,7 @@ public class NormalTests {
             this.id = identification;
         }
 
+        // getters:
         Double getCpu() {
             return cpu;
         }
@@ -190,6 +242,48 @@ public class NormalTests {
         Double getWrite() {
             return writes;
         }
+
+        int getId() {
+            return id;
+        }
+
+        // setters:
+        int getGroupCpu() {
+            return GCpu;
+        }
+
+        int getGroupElapsed() {
+            return GElapsed;
+        }
+
+        int getGroupWrite() {
+            return GWrite;
+        }
+
+        // setters:
+        void setGroupCpu(int a) {
+            GCpu = a;
+        }
+
+        void setGroupElapsed(int a) {
+            GElapsed = a;
+        }
+
+        void setGroupWrite(int a) {
+            GWrite = a;
+        }
+
+        void setCpu(Double c) {
+            this.cpu = c;
+        }
+
+        void setWrite(Double w) {
+            this.writes = w;
+        }
+
+        void setElapsed(Double e) {
+            this.elapsed = e;
+        }
     }
 
     // Normal distribution test:
@@ -198,6 +292,13 @@ public class NormalTests {
         System.out.println("Normal Tests");
 
         Executions a = new Executions();
+        // Shuffle the runs:
+        a.shuffle();
+        // link of cpu time and elapsed time:
+        a.connect();
+
+        ArrayList<Run> normal = new ArrayList<>();
+        ArrayList<Run> abnormal = new ArrayList<>();
 
         // Show:
         if (printFlag > 0) {
@@ -217,6 +318,48 @@ public class NormalTests {
             System.out.println(resultET);
             System.out.println(resultCPU);
             System.out.println(resultWrite);
+        }
+
+        ArrayList<Run> Runs = a.getRuns();
+
+        // Set the groups for the runs:
+        for (int i = 0; i < Runs.size(); i++) {
+            Run eachRun = Runs.get(i);
+
+            int groupET = findGroup(resultET, eachRun.getElapsed());
+            int groupCPU = findGroup(resultCPU, eachRun.getCpu());
+            int groupWrite = findGroup(resultWrite, eachRun.getWrite());
+
+            eachRun.setGroupElapsed(groupET);
+            eachRun.setGroupCpu(groupCPU);
+            eachRun.setGroupWrite(groupWrite);
+
+            // fast:
+            if (eachRun.getGroupElapsed() == 1) {
+                normal.add(eachRun);
+            } // slow:
+            else {
+                abnormal.add(eachRun);
+            }
+
+        }
+
+        if (printFlag > 0) {
+            // System.out.println("Run ids \t elapsed cpu write \t");
+            for (int i = 0; i < Runs.size(); i++) {
+                Run eachRun = Runs.get(i);
+                // System.out.println("Run id \t" + eachRun.getId() + " \t " +
+                // eachRun.getGroupElapsed() + " \t " + eachRun.getGroupCpu() +
+                // " \t " + eachRun.getGroupWrite());
+            }
+        }
+
+        // Run through the abnormal runs:
+        System.out.println("Abnormal activity");
+        System.out.println("Run id \t\t elapsed cpu write");
+        for (int i = 0; i < abnormal.size(); i++) {
+            Run eachRun = abnormal.get(i);
+            System.out.println("Run id \t" + eachRun.getId() + " \t " + eachRun.getGroupElapsed() + "  \t " + eachRun.getGroupCpu() + " \t " + eachRun.getGroupWrite());
         }
     }
 
@@ -239,6 +382,20 @@ public class NormalTests {
 
         // return the group:
         return group;
+    }
+
+    // HashMap add:
+    private static int findGroup(ArrayList<ArrayList<Double>> array, double number) {
+        ArrayList<Double> temp;
+        for (int i = 0; i < array.size(); i++) {
+            temp = array.get(i);
+            for (int j = 0; j < temp.size(); j++) {
+                if (temp.get(j) == number) {
+                    return i + 1;
+                }
+            }
+        }
+        return -1;
     }
 
     // HashMap add:
