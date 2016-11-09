3001fa9a1ccaf399b9093571802dbbfe9318877e
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-11-08 19:36:35 -0500

    several tests with different groups and heuristics evaluations

--- analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/NormalTests.java
+++ analysis/org.eclipse.tracecompass.analysis.os.linux.core/src/org/eclipse/tracecompass/internal/analysis/os/linux/core/profile/NormalTests.java
@@ -27,6 +27,7 @@ public class NormalTests {
 
         ArrayList<Run> runs;
 
+        // Constructors:
         Executions() {
             id = 1;
 
@@ -37,6 +38,43 @@ public class NormalTests {
             fulfilDistributionWrite();
 
             runs = createsRuns();
+        }
+
+        Executions(int i) {
+            id = 1;
+
+            fulfilNotRandom(distributionET, hmET, 3);
+
+            fulfilDistributionCPU();
+
+            fulfilDistributionWrite();
+
+            runs = createsRuns();
+        }
+
+        private void fulfilNotRandom(ArrayList<Double> distribution, LinkedHashMap<Integer, ArrayList<Double>> hm, int ngroups) {
+
+            //create 2 groups distribution:
+            if (ngroups == 2) {
+                double[] aux = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150 };
+                int[] type = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+                // first Normal:
+                for (int i = 0; i < this.tam * 2; i++) {
+                    distribution.add(aux[i]);
+                    hashAdd(hm, type[i], aux[i]);
+                    // hm.put(1, aux);
+                }
+            }
+            else{//create 3 groups distribution:
+                double[] aux = { 1, 2, 3, 4, 5, 6, 7, 600, 700, 800, 900,1000,1100, 1200,1300, 1200000, 1300000, 1400000, 1500000, 1600000};
+                int[] type = { 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3 };
+                // first Normal:
+                for (int i = 0; i < this.tam * 2; i++) {
+                    distribution.add(aux[i]);
+                    hashAdd(hm, type[i], aux[i]);
+                    // hm.put(1, aux);
+                }
+            }
         }
 
         private void fulfilDistributionET() {
@@ -194,7 +232,7 @@ public class NormalTests {
 
         // This function run through the runs and connect the distributions (cpu
         // time and elapsed time):
-        public void connect() {
+        public void connect1() {
 
             distributionET = new ArrayList<>();
             // run:
@@ -203,6 +241,25 @@ public class NormalTests {
                 Double cpu = eachRun.getCpu();
                 eachRun.setElapsed(cpu * 2);
                 distributionET.add(cpu * 2);
+
+                // hashAdd(hm, type, aux);
+
+            }
+
+        }
+
+        // This function run through the runs and connect the distributions
+        // (write
+        // time and elapsed time):
+        public void connect2() {
+
+            distributionET = new ArrayList<>();
+            // run:
+            for (int i = 0; i < runs.size(); i++) {
+                Run eachRun = runs.get(i);
+                Double write = eachRun.getWrite();
+                eachRun.setElapsed(write * 2);
+                distributionET.add(write * 2);
 
                 // hashAdd(hm, type, aux);
 
@@ -285,26 +342,116 @@ public class NormalTests {
             this.elapsed = e;
         }
     }
+    // This function correlates the groups:
+    private static void createHistograms(ArrayList<Run> array, int gCPU, int gWrite) {
+        System.out.println("Create histograms");
+        System.out.println("Run id \t\t elapsed cpu write");
+
+        int[] histogramGroupsCPU = new int[gCPU + 1];
+        int[] histogramGroupsWrite = new int[gWrite + 1];
+
+        for (int i = 0; i < array.size(); i++) {
+            Run eachRun = array.get(i);
+            System.out.println("Run id \t" + eachRun.getId() + " \t " + eachRun.getGroupElapsed() + "  \t " + eachRun.getGroupCpu() + " \t " + eachRun.getGroupWrite());
+            int groupC = eachRun.getGroupCpu();
+            int groupW = eachRun.getGroupWrite();
+
+            // Do a histogram according to the group:
+            histogramGroupsCPU[groupC] += 1;
+            histogramGroupsWrite[groupW] += 1;
+
+        }
+
+        System.out.println("Cpu");
+        for (int i = 1; i < histogramGroupsCPU.length; i++) {
+            double perc = 0.0;
+            if (histogramGroupsCPU[i] > 0) {
+                perc = ((double) histogramGroupsCPU[i] * 100 / array.size());
+            }
+            System.out.println(perc + "% is in group " + i);
+        }
+        System.out.println("Write");
+        for (int i = 1; i < histogramGroupsWrite.length; i++) {
+            double perc = 0.0;
+            if (histogramGroupsWrite[i] > 0) {
+                perc = ((double) histogramGroupsWrite[i] * 100 / array.size());
+            }
+            System.out.println(perc + "% is in group " + i);
+        }
+
+    }
+
+    // This function classify an array with the best k:
+    private static ArrayList<ArrayList<Double>> classification(ArrayList<Double> array, int printFlag) {
+        ArrayList<Double> resultSSE = new ArrayList<>();
+        // learn the best k
+        for (int j = 2; j < array.size(); j++) {
+            // K, size and arraylist of numbers
+            ArrayList<ArrayList<Double>> group = KMean.executeD(j, array);
+            resultSSE.add(KMean.ElbowMethodD(group, 0));
+        }
+        // heuristic validation in learning:
+        int bestk = (KMean.calculateBestK(resultSSE) + 1);
+        if (printFlag > 0) {
+            System.out.println("Best number of groups: " + bestk);
+        }
+        // applies the best k
+        ArrayList<ArrayList<Double>> group = KMean.executeD(bestk, array);
+
+        // return the group:
+        return group;
+    }
+
+    // HashMap add:
+    private static int findGroup(ArrayList<ArrayList<Double>> array, double number) {
+        ArrayList<Double> temp;
+        for (int i = 0; i < array.size(); i++) {
+            temp = array.get(i);
+            for (int j = 0; j < temp.size(); j++) {
+                if (temp.get(j) == number) {
+                    return i + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // HashMap add:
+    private static void hashAdd(LinkedHashMap<Integer, ArrayList<Double>> hm, Integer key, Double aux) {
+
+        // contains:
+        if (hm.containsKey(key)) {
+
+            ArrayList<Double> array = hm.get(key);
+            if (array != null) {
+                array.add(aux);
+                hm.put(key, array);
+            }
+        } else {
+            ArrayList<Double> array = new ArrayList<>();
+            array.add(aux);
+            hm.put(key, array);
+        }
+    }
 
     // Normal distribution test:
     public static void testNormal(int printFlag) {
 
         System.out.println("Normal Tests");
 
-        Executions a = new Executions();
+        Executions a = new Executions(1);
         // Shuffle the runs:
         a.shuffle();
         // link of cpu time and elapsed time:
-        a.connect();
+        //a.connect1();
 
-        ArrayList<Run> normal = new ArrayList<>();
-        ArrayList<Run> abnormal = new ArrayList<>();
+        ArrayList<ArrayList<Run>> merge = new ArrayList<>();
+        ArrayList<Run> temp;
 
         ArrayList<ArrayList<Double>> resultET;
         ArrayList<ArrayList<Double>> resultCPU;
         ArrayList<ArrayList<Double>> resultWrite;
 
-        int[] histogramGroups = new int[3];
         int ngET;
         int ngCPU;
         int ngW;
@@ -352,17 +499,19 @@ public class NormalTests {
         }
 
         // Histogram:
-        for (int i = 0; i < Runs.size(); i++) {
-            Run eachRun = Runs.get(i);
-            // fast:
-            if (eachRun.getGroupElapsed() == 1) {
-                normal.add(eachRun);
-            } // slow:
-            else {
-                abnormal.add(eachRun);
+        for (int j = 0; j <= ngET; j++) {
+            temp = new ArrayList<>();
+            for (int i = 0; i < Runs.size(); i++) {
+                Run eachRun = Runs.get(i);
+                // fast:
+                if (eachRun.getGroupElapsed() == j) {
+                    temp.add(eachRun);
+                }
             }
+            merge.add(temp);
         }
 
+        // Print:
         if (printFlag > 0) {
             // System.out.println("Run ids \t elapsed cpu write \t");
             for (int i = 0; i < Runs.size(); i++) {
@@ -373,100 +522,11 @@ public class NormalTests {
             }
         }
 
-        createHistograms(normal, ngCPU, ngW);
-        createHistograms(abnormal, ngCPU, ngW);
-
-    }
-
-    // This function correlates the groups:
-    private static void createHistograms(ArrayList<Run> array, int gCPU, int gWrite) {
-        System.out.println("Create histograms");
-        System.out.println("Run id \t\t elapsed cpu write");
-
-        int[] histogramGroupsCPU = new int[gCPU + 1];
-        int[] histogramGroupsWrite = new int[gWrite + 1];
-
-        for (int i = 0; i < array.size(); i++) {
-            Run eachRun = array.get(i);
-            System.out.println("Run id \t" + eachRun.getId() + " \t " + eachRun.getGroupElapsed() + "  \t " + eachRun.getGroupCpu() + " \t " + eachRun.getGroupWrite());
-            int groupC = eachRun.getGroupCpu();
-            int groupW = eachRun.getGroupWrite();
-
-            // Do a histogram according to the group:
-            histogramGroupsCPU[groupC] += 1;
-            histogramGroupsWrite[groupW] += 1;
-
-        }
-
-        System.out.println("Cpu");
-        for (int i = 1; i < histogramGroupsCPU.length; i++) {
-            if (histogramGroupsCPU[i] > 0) {
-                System.out.println("Group " + i + " quantity: " + histogramGroupsCPU[i] + " Percentage: " + ((double) histogramGroupsCPU[i] * 100 / array.size()) + " %");
-            } else {
-                System.out.println("Group " + i + " quantity: " + histogramGroupsCPU[i] + " Percentage: 0%");
-            }
-        }
-        System.out.println("Write");
-        for (int i = 1; i < histogramGroupsWrite.length; i++) {
-            if (histogramGroupsWrite[i] > 0) {
-                System.out.println("Group " + i + " quantity: " + histogramGroupsWrite[i] + " Percentage: " + ((double) histogramGroupsWrite[i] * 100 / array.size()) + " %");
-            } else {
-                System.out.println("Group " + i + " quantity: " + histogramGroupsWrite[i] + " Percentage: 0%");
-            }
+        // See the relationship in each group:
+        for (int j = 0; j <= ngET; j++) {
+            createHistograms(merge.get(j), ngCPU, ngW);
         }
 
     }
 
-    // This function classify an array with the best k:
-    private static ArrayList<ArrayList<Double>> classification(ArrayList<Double> array, int printFlag) {
-        ArrayList<Double> resultSSE = new ArrayList<>();
-        // testing all the ks:
-        for (int j = 1; j < array.size(); j++) {
-            // K, size and arraylist of numbers
-            ArrayList<ArrayList<Double>> group = KMean.executeD(j, array);
-            resultSSE.add(KMean.ElbowMethodD(group, 0));
-        }
-        // biggest gap:
-        int bestk = (KMean.calculateBestK(resultSSE) + 1);
-        if (printFlag > 0) {
-            System.out.println("Best number of groups: " + bestk);
-        }
-
-        ArrayList<ArrayList<Double>> group = KMean.executeD(bestk, array);
-
-        // return the group:
-        return group;
-    }
-
-    // HashMap add:
-    private static int findGroup(ArrayList<ArrayList<Double>> array, double number) {
-        ArrayList<Double> temp;
-        for (int i = 0; i < array.size(); i++) {
-            temp = array.get(i);
-            for (int j = 0; j < temp.size(); j++) {
-                if (temp.get(j) == number) {
-                    return i + 1;
-                }
-            }
-        }
-        return -1;
-    }
-
-    // HashMap add:
-    private static void hashAdd(LinkedHashMap<Integer, ArrayList<Double>> hm, Integer key, Double aux) {
-
-        // contains:
-        if (hm.containsKey(key)) {
-
-            ArrayList<Double> array = hm.get(key);
-            if (array != null) {
-                array.add(aux);
-                hm.put(key, array);
-            }
-        } else {
-            ArrayList<Double> array = new ArrayList<>();
-            array.add(aux);
-            hm.put(key, array);
-        }
-    }
 }
